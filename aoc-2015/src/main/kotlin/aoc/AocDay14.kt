bf1ee099b50d4f7359dc5a571c39d13f11e5c851
Elisha Peterson <triathematician@gmail.com>
2023-12-05 07:21:33 -0500

    aoc 2015 days 14-15

--- aoc-2015/src/main/kotlin/aoc/AocDay14.kt
+++ aoc-2015/src/main/kotlin/aoc/AocDay14.kt
@@ -1,12 +1,49 @@
 package aoc
 
-class AocDay0: AocDay(0) {
-    companion object { @JvmStatic fun main(args: Array<String>) { AocDay0().run() } }
+import aoc.util.chunk
+import aoc.util.chunkint
+
+class AocDay14: AocDay(14) {
+    companion object { @JvmStatic fun main(args: Array<String>) { AocDay14().run() } }
 
     override val testinput = """
-        
+        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
     """.trimIndent().lines()
 
-    override fun calc1(input: List<String>) = null
-    override fun calc2(input: List<String>) = null
+    class Reindeer(val name: String, val speed: Int, val st: Int, val rt: Int) {
+        var dist = 0
+        override fun toString() = "$name:${dist}km"
+        /** Advance reindeer at given time, advancing by speed of flying or 0 if resting. */
+        fun advance(t: Int): Int {
+            val rem = t % (st + rt)
+            val flying = rem < st
+            if (flying)
+                dist += speed
+            return dist
+        }
+        fun traveled(t: Int): Int {
+            val cycles = t / (st + rt)
+            val rem = t % (st + rt)
+            return speed*st*cycles + speed*minOf(st, rem)
+        }
+    }
+
+    fun String.parse() = Reindeer(chunk(0), chunkint(3), chunkint(6), chunkint(13))
+
+    override fun calc1(input: List<String>): Int {
+        val rd = input.map { it.parse() }
+        return rd.maxOf { it.traveled(2503) }
+    }
+    override fun calc2(input: List<String>): Int {
+        val rd = input.map { it.parse() }
+        val points = rd.associateWith { 0 }.toMutableMap()
+        (0..2502).forEach { t ->
+            val dists = rd.map { it.advance(t) }
+            val max = dists.maxOrNull()!!
+            rd.filter { it.dist == max }.forEach { points[it] = points[it]!! + 1 }
+            println("$t: $rd $points")
+        }
+        return points.values.maxOrNull()!!
+    }
 }
--- aoc-2015/src/main/kotlin/aoc/AocDay15.kt
+++ aoc-2015/src/main/kotlin/aoc/AocDay15.kt
@@ -1,12 +1,45 @@
 package aoc
 
-class AocDay0: AocDay(0) {
-    companion object { @JvmStatic fun main(args: Array<String>) { AocDay0().run() } }
+import aoc.util.chunk
+import aoc.util.chunkint
+
+class AocDay15: AocDay(15) {
+    companion object { @JvmStatic fun main(args: Array<String>) { AocDay15().run() } }
 
     override val testinput = """
-        
+        Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+        Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
     """.trimIndent().lines()
 
-    override fun calc1(input: List<String>) = null
-    override fun calc2(input: List<String>) = null
+    class Ingr(val name: String, val c: Int, val d: Int, val f: Int, val t: Int, val cal: Int)
+    class Cookie(val ingr: Map<Ingr, Int>) {
+        val c = ingr.map { it.key.c * it.value }.sum().coerceAtLeast(0)
+        val d = ingr.map { it.key.d * it.value }.sum().coerceAtLeast(0)
+        val f = ingr.map { it.key.f * it.value }.sum().coerceAtLeast(0)
+        val t = ingr.map { it.key.t * it.value }.sum().coerceAtLeast(0)
+        val cal = ingr.map { it.key.cal * it.value }.sum()
+        fun score() = c*d*f*t
+    }
+
+    fun String.chunkints(n: Int) = chunk(n).substringBefore(",").toInt()
+    fun String.parse() = Ingr(chunk(0).dropLast(1), chunkints(2), chunkints(4), chunkints(6), chunkints(8), chunkints(10))
+
+    fun bestScore(ingrs: List<Ingr>, tsp: Int, c: Cookie, cals: Int?): Int {
+        if (ingrs.size == 1) {
+            val c2 = Cookie(c.ingr + (ingrs[0] to tsp))
+            return if (cals != null && c2.cal != cals) 0 else c2.score()
+        }
+        return (0..tsp).maxOf {
+            bestScore(ingrs.drop(1), tsp - it, Cookie(c.ingr + (ingrs[0] to it)), cals)
+        }
+    }
+
+    override fun calc1(input: List<String>): Int {
+        val ingrs = input.map { it.parse() }
+        return bestScore(ingrs, 100, Cookie(mapOf()), null)
+    }
+    override fun calc2(input: List<String>): Int {
+        val ingrs = input.map { it.parse() }
+        return bestScore(ingrs, 100, Cookie(mapOf()), 500)
+    }
 }
