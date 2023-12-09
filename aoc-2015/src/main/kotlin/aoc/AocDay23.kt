b297e2d6ed1dce290724b39c93306f6d881eef1c
Elisha Peterson <triathematician@gmail.com>
2023-12-09 16:07:24 -0500

    aoc 2016 days 23-24

--- aoc-2015/src/main/kotlin/aoc/AocDay23.kt
+++ aoc-2015/src/main/kotlin/aoc/AocDay23.kt
@@ -4,9 +4,54 @@ class AocDay23: AocDay(23) {
     companion object { @JvmStatic fun main(args: Array<String>) { AocDay23().run() } }
 
     override val testinput = """
-        
+        inc a
+        jio a, +2
+        tpl a
+        inc a
     """.trimIndent().lines()
 
-    override fun calc1(input: List<String>) = null
-    override fun calc2(input: List<String>) = null
+    fun String.parse(): (State) -> Unit = when (this) {
+        "inc a" -> ::incA
+        "inc b" -> ::incB
+        "tpl a" -> ::tplA
+        "tpl b" -> ::tplB
+        "hlf a" -> ::hlfA
+        "hlf b" -> ::hlfB
+        "jmp +2" -> { state -> state.line += 2-1 }
+        "jmp -7" -> { state -> state.line += -7-1 }
+        "jmp +19" -> { state -> state.line += 19-1 }
+        "jio a, +22" -> { state -> if (state.a == 1) state.line += 22-1  }
+        "jio a, +8" -> { state -> if (state.a == 1) state.line += 8-1 }
+        "jio a, +2" -> { state -> if (state.a == 1) state.line += 2-1 }
+        "jie a, +4" -> { state -> if (state.a % 2 == 0) state.line += 4-1 }
+        else -> throw IllegalArgumentException("Unknown instruction: $this")
+    }
+
+    class State(var a: Int = 0, var b: Int = 0, var line: Int = 0)
+
+    fun incA(state: State) { state.a++ }
+    fun incB(state: State) { state.b++ }
+    fun tplA(state: State) { state.a *= 3 }
+    fun tplB(state: State) { state.b *= 3 }
+    fun hlfA(state: State) { state.a /= 2 }
+    fun hlfB(state: State) { state.b /= 2 }
+
+    override fun calc1(input: List<String>): Int {
+        val instructs = input.map { it.parse() }
+        val state = State()
+        while (state.line < instructs.size) {
+            instructs[state.line](state)
+            state.line++
+        }
+        return state.b
+    }
+    override fun calc2(input: List<String>): Int {
+        val instructs = input.map { it.parse() }
+        val state = State(a = 1)
+        while (state.line < instructs.size) {
+            instructs[state.line](state)
+            state.line++
+        }
+        return state.b
+    }
 }
--- aoc-2015/src/main/kotlin/aoc/AocDay24.kt
+++ aoc-2015/src/main/kotlin/aoc/AocDay24.kt
@@ -4,9 +4,72 @@ class AocDay24: AocDay(24) {
     companion object { @JvmStatic fun main(args: Array<String>) { AocDay24().run() } }
 
     override val testinput = """
-        
+        1
+        2
+        3
+        4
+        5
+        7
+        8
+        9
+        10
+        11
     """.trimIndent().lines()
 
-    override fun calc1(input: List<String>) = null
-    override fun calc2(input: List<String>) = null
+    fun List<String>.parse() = map { it.toInt() }
+
+    override fun calc1(input: List<String>): Long {
+        return 0
+        val pkgs = input.parse().toSet()
+        val target = pkgs.sum() / 3
+        val combos = pkgs.subsetsSummingTo(target)
+        val combosBySize = combos.groupBy { it.size }
+        combosBySize.entries.sortedBy { it.key }.forEach { (count, combos) ->
+            print("Combos of size $count: ${combos.size}")
+            val valid = combos.filter { (pkgs - it.toSet()).dividesIntoGroups(target, 2) }
+            println(", valid: ${valid.size}")
+            if (valid.isNotEmpty())
+                return valid.minOf { it.fold(1L) { acc, i -> acc * i } }
+        }
+        return -1
+    }
+
+    override fun calc2(input: List<String>): Long {
+        val pkgs = input.parse().toSet()
+        val target = pkgs.sum() / 4
+        val combos = pkgs.subsetsSummingTo(target, maxSize = 7)
+        val combosBySize = combos.groupBy { it.size }
+        combosBySize.entries.sortedBy { it.key }.forEach { (count, combos) ->
+            print("Combos of size $count: ${combos.size}")
+            val valid = combos.filter { (pkgs - it.toSet()).dividesIntoGroups(target, 3) }
+            println(", valid: ${valid.size}")
+            if (valid.isNotEmpty())
+                return valid.minOf { it.fold(1L) { acc, i -> acc * i } }
+        }
+        return -1
+    }
+
+    fun Set<Int>.dividesIntoGroups(target: Int, groups: Int): Boolean {
+        if (groups == 1)
+            return sum() == target
+        else if (groups == 2)
+            return subsetsSummingTo(target).isNotEmpty()
+        else {
+            val s2 = subsetsSummingTo(target)
+            return s2.any { (this - it.toSet()).dividesIntoGroups(target, 2) }
+        }
+    }
+
+    fun Collection<Int>.subsetsSummingTo(target: Int, maxSize: Int? = null): List<List<Int>> {
+        if (isEmpty())
+            return listOf()
+
+        val first = first()
+        val withFirst = when {
+            first < target -> drop(1).subsetsSummingTo(target - first, maxSize?.let { it - 1}).map { it + first }
+            first == target -> listOf(listOf(first))
+            else -> listOf()
+        }
+        return withFirst + drop(1).subsetsSummingTo(target, maxSize)
+    }
 }
