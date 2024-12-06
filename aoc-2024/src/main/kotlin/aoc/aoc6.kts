7a9d71fe24ebc026e0f52ea8ba3043abd848d964
Elisha Peterson <triathematician@gmail.com>
2024-12-06 08:00:32 -0500

    aoc 2024 day 6 wip

--- aoc-2024/src/main/kotlin/aoc/aoc6.kts
+++ aoc-2024/src/main/kotlin/aoc/aoc6.kts
@@ -1,18 +1,74 @@
 import aoc.util.AocParser.Companion.parselines
 import aoc.*
 import aoc.getDayInput
+import aoc.util.*
 
 val testInput = """
-
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
 """.parselines
 
 // part 1
 
-fun List<String>.part1(): Int = 0
+fun CharGrid.part1(): Int {
+//    val coords = allIndices2()
+    val pos = findCoords2 { it == '^' }.values.first().first()
+    return path(pos, NORTH).first.map { it.pos }.toSet().size
+}
+
+data class PosDir(val pos: Coord, val dir: Coord)
+
+/** Get set of coords until leaving the map, or repeating. */
+fun CharGrid.path(p: Coord, d: Coord): Pair<Set<PosDir>, Boolean> {
+    val coords = allIndices2()
+    var pos = PosDir(p, d)
+    val traveled = mutableListOf<PosDir>()
+    while (true) {
+        traveled += pos
+        val next = pos.pos + pos.dir
+        if (next in coords && at(next) == '#') {
+            pos = PosDir(pos.pos, when (pos.dir) {
+                NORTH -> EAST
+                EAST -> SOUTH
+                SOUTH -> WEST
+                WEST -> NORTH
+                else -> error("Invalid direction")
+            })
+        } else if (next in coords) {
+            pos = PosDir(next, pos.dir)
+        } else {
+            return traveled.toSet() to false
+        }
+        if (pos in traveled) {
+            return traveled.toSet() to true
+        }
+    }
+}
 
 // part 2
 
-fun List<String>.part2() = 0
+fun CharGrid.part2(): Int {
+    val pos = findCoords2 { it == '^' }.values.first().first()
+    // only makes sense for obstacles to be somewhere in current path
+    val openPos = path(pos, NORTH).first.map { it.pos }.toSet()
+    return openPos.count {
+        loopFrom(pos, it)
+    }
+}
+
+fun CharGrid.loopFrom(pos: Coord, obstacle: Coord): Boolean {
+    val cg2 = toMutableList().map { it.toMutableList() }
+    cg2[obstacle.y][obstacle.x] = '#'
+    return cg2.map { String(it.toCharArray()) }.path(pos, NORTH).second
+}
 
 // calculate answers
 
