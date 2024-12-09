f854d3ff8eb1121ad852b4a060d75bbf90fbd82e
Elisha Peterson <triathematician@gmail.com>
2024-12-09 08:07:35 -0500

    aoc 2024 day 9 and improved stats

--- aoc-2024/src/main/kotlin/aoc/aoc2024.kts
+++ aoc-2024/src/main/kotlin/aoc/aoc2024.kts
@@ -1,15 +1,16 @@
-import aoc.report.randomColors
 import aoc.util.*
 import java.io.File
 import java.time.LocalDate
 import java.time.Month
 
+private val YEAR = 2024
 val COLORA = ANSI_B_BLUE
 val COLORB = ANSI_B_PURPLE
 val COLORC = ANSI_B_PINK
 
 val leaders = """
 --100th Best Times for Each Puzzle--
+Day9: 5:51/14:05 (defrag checksums)
 Day8: 3:57/7:12 (antenna grid)
 Day7: 1:59/3:47 (inserting operators)
 Day6: 2:40/8:53 (paths in grid)
@@ -22,6 +23,7 @@ Day1: 1:24/2:31 (sort similarity)
 
 val personalstats = """
 Day       Time   Rank   Score       Time    Rank  Score InputFile
+  9   07:23:54   25619      0   07:32:19   16224      0  06:46:11
   8   07:35:39   26750      0   07:37:39   24005      0  07:26:27
   7   08:01:32   29681      0   08:10:16   27082      0  07:31:48
   6   07:04:43   34915      0   31:50:42   46903      0  06:57:41
@@ -29,54 +31,40 @@ Day       Time   Rank   Score       Time    Rank  Score InputFile
   4   07:33:53   41264      0   07:43:03   35252      0  07:24:02
   3   09:53:26   65231      0   10:02:58   54671      0  09:49:45
   2   12:10:20   78024      0   12:13:02   55710      0  12:03:13
-  1   36:10:00  135368      0   36:10:00  126387      0  35:48:12
+  1   35:55:16  135368      0   35:59:44  126387      0  35:48:12
 """.trimIndent().lines().drop(1).map {
     val columns = it.trim().split("\\s+".toRegex())
     Data(columns[0].toInt(), columns[1], columns[2].toInt(), columns[4], columns[5].toInt(), columns[7])
 }
 
 personalstats.printChart()
-
-printHeader("Part 1 Ranks")
-personalstats.map { it.rank }.sorted().printSummary()
-
-printHeader("Part 2 Ranks")
-personalstats.map { it.rank2 }.sorted().printSummary()
-
-printHeader("Number of Ranks Dropped")
-// drop 1 invalid time this year
-personalstats.map { it.rank - it.rank2 }.sortedDescending().drop(1).improvementSummary()
-
-printHeader("Time to Solve Part 1")
-personalstats.map { it.timePart1 }.sorted().map { it.hms() }.printSummary()
-
-printHeader("Time to Solve Part 2")
-// drop 1 invalid time this year
-personalstats.map { it.timePart2 }.sorted().drop(1).map { it.hms() }.printSummary()
-
-printHeader("Time to Solve Both")
-personalstats.map { it.timeBoth }.sorted().map { it.hms() }.printSummary()
+personalstats.printRankStats()
+personalstats.printTimeStats()
 
 printHeader("Updating Files")
+updateFiles()
+println("Done!")
 
-val rootPath = File(File("").absolutePath.substringBeforeLast("aoc-2024")+"aoc-2024")
-val codePath = File(rootPath, "src/main/kotlin/aoc/")
-val day = LocalDate.now().let {
-    if (it.year == 2024 && it.month == Month.DECEMBER) minOf(it.dayOfMonth + 1, 25) else -1
-}
-if (day != -1) {
-    val codeTemplate = File(codePath, "aoc_.kts").readText().replace("day = 0", "day = $day")
-    val inputCode = File(codePath, "aoc$day.kts")
-    if (!inputCode.exists()) {
-        inputCode.writeText(codeTemplate)
-        println("Created file ${inputCode.name}")
+fun updateFiles() {
+    val rootPath = File(File("").absolutePath.substringBeforeLast("aoc-$YEAR") + "aoc-$YEAR")
+    val codePath = File(rootPath, "src/main/kotlin/aoc/")
+    val day = LocalDate.now().let {
+        if (it.year == YEAR && it.month == Month.DECEMBER) minOf(it.dayOfMonth + 1, 25) else -1
+    }
+    if (day != -1) {
+        val codeTemplate = File(codePath, "aoc_.kts").readText().replace("day = 0", "day = $day")
+        val inputCode = File(codePath, "aoc$day.kts")
+        if (!inputCode.exists()) {
+            inputCode.writeText(codeTemplate)
+            println("Created file ${inputCode.name}")
+        }
     }
 }
 
 //region UTILS
 
 fun printHeader(str: String) {
-    println("\n---${str.randomColors(COLORA, COLORB, COLORC)}${ANSI_RESET}---")
+    println("\n---$str${ANSI_RESET}---")
 }
 
 fun List<*>.printSummary() {
@@ -89,6 +77,63 @@ fun List<*>.improvementSummary() {
     println("${ANSI_LIGHT_GREEN}Largest drop: $ANSI_BOLD${first()}${ANSI_RESET}")
     println("${ANSI_LIGHT_YELLOW}Median drop:  $ANSI_BOLD${this[size / 2]}${ANSI_RESET}")
     println("${ANSI_BROWN}Lowest drop:  $ANSI_BOLD${last()}${ANSI_RESET}")
+}
+
+fun List<Int>.printHistogram(min: Int? = 0, bucketSize: Int = 1000) {
+    if (isEmpty()) {
+        println("EMPTY")
+        return
+    }
+    val min = min ?: min()
+    val max = max()
+    val range = max - min
+    val buckets = IntArray(range / bucketSize + 1)
+    forEach { buckets[(it - min) / bucketSize]++ }
+    buckets.forEachIndexed { i, count ->
+        val rangeStart = min + i * bucketSize
+        val rangeEnd = rangeStart + bucketSize
+        val bar = "*".repeat(count * 3)
+        println("%5d - %5d: $bar".format(rangeStart, rangeEnd-1))
+    }
+}
+
+fun List<Int>.printHistogramMinutes(min: Int? = 0, bucketSize: Int = 1, overflow: Int = 60) {
+    if (isEmpty()) {
+        println("EMPTY")
+        return
+    }
+    val numMin = min()
+    val numMed = sorted()[size / 2]
+    val numMax = max()
+    val useMin = min ?: (numMin / 60)
+    val useMax = filter { it < overflow * 60 }.maxOrNull()?.let { it / 60 } ?: useMin
+    val range = useMax - useMin
+    val buckets = IntArray(range / bucketSize + 1)
+    var nOver = 0
+    forEach {
+        val bin = (it - useMin) / (bucketSize * 60)
+        if (bin in buckets.indices)
+            buckets[bin]++
+        else
+            nOver++
+    }
+    buckets.forEachIndexed { i, count ->
+        val rangeStart = useMin + i * bucketSize
+        val rangeEnd = rangeStart + bucketSize
+        val bar = "*".repeat(count * 3)
+        print("%2d:00 - %2d:59: $bar".format(rangeStart, rangeEnd-1))
+        if (numMin/60 in rangeStart until rangeEnd)
+            print("  $ANSI_BOLD${ANSI_LIGHT_GREEN}BEST: ${numMin.minSec()}$ANSI_RESET")
+        if (numMed/60 in rangeStart until rangeEnd)
+            print("  $ANSI_BOLD${ANSI_LIGHT_YELLOW}MEDIAN: ${numMed.minSec()}$ANSI_RESET")
+        if (numMax/60 in rangeStart until rangeEnd)
+            print("  $ANSI_BOLD${ANSI_BROWN}WORST: ${numMax.minSec()}$ANSI_RESET")
+        println()
+    }
+    print("     >= %2d:00: ${"***".repeat(nOver)}".format(useMin + buckets.size * bucketSize))
+    if (numMax >= overflow * 60)
+        print("  $ANSI_BOLD${ANSI_BROWN}WORST: ${numMax.hms()}$ANSI_RESET")
+    println()
 }
 
 fun List<Data>.printChart() {
@@ -121,6 +166,34 @@ fun List<Data>.printChart() {
             .colorStars()
         println(line)
     }
+}
+
+fun List<Data>.printRankStats() {
+    printHeader("Part 1 Ranks")
+    map { it.rank }.sorted().printSummary()
+    map { it.rank }.printHistogram(bucketSize = 10000)
+
+    printHeader("Part 2 Ranks")
+    map { it.rank2 }.sorted().printSummary()
+    map { it.rank2 }.printHistogram(bucketSize = 10000)
+
+    printHeader("Number of Ranks Dropped")
+    map { it.rank - it.rank2 }.sortedDescending().improvementSummary()
+    map { it.rank - it.rank2 }.printHistogram(min = -15000, bucketSize = 5000)
+}
+
+fun List<Data>.printTimeStats() {
+    printHeader("Time to Solve Part 1")
+//    map { it.timePart1 }.sorted().map { it.hms() }.printSummary()
+    map { it.timePart1 }.printHistogramMinutes(bucketSize = 5, overflow = 60)
+
+    printHeader("Time to Solve Part 2")
+//    map { it.timePart2 }.sorted().map { it.hms() }.printSummary()
+    map { it.timePart2 }.printHistogramMinutes(bucketSize = 5, overflow = 60)
+
+    printHeader("Time to Solve Both")
+//    map { it.timeBoth }.sorted().map { it.hms() }.printSummary()
+    map { it.timeBoth }.printHistogramMinutes(bucketSize = 5, overflow = 60)
 }
 
 fun String.colorStars(): String {
@@ -161,5 +234,7 @@ fun Int.hms() = if (this >= 3600) {
 } else {
     "%5d:%02d".format(this / 60, this % 60)
 }
+
+fun Int.minSec() = "%2d:%02d".format(this / 60, this % 60)
 
 //endregion
--- aoc-2024/src/main/kotlin/aoc/aoc9.kts
+++ aoc-2024/src/main/kotlin/aoc/aoc9.kts
@@ -3,16 +3,90 @@ import aoc.*
 import aoc.getDayInput
 
 val testInput = """
-
+2333133121414131402
 """.parselines
+
+data class Data(val start: Int, val id: Int, val n: Int) {
+    val checksum: Long = if (id == -1) 0 else
+            (1..n).sumOf { (start + it - 1) * id.toLong() }
+}
 
 // part 1
 
-fun List<String>.part1(): Int = 0
+fun String.parse(): List<Data> {
+    val data = mutableListOf<Data>()
+    var pos = 0
+    var id = 0
+    forEachIndexed { i, c ->
+        val digit = c.toString().toInt()
+        if (i % 2 == 0) {
+            data += Data(pos, id, digit)
+            id++
+        } else {
+            data += Data(pos, -1, digit)
+        }
+        pos += digit
+    }
+    return data
+}
+
+fun List<String>.part1(): Long {
+    return first().parse().compactify().sumOf { it.checksum }
+}
+
+fun List<Data>.compactify(): List<Data> {
+    var result = this.filter { it.n > 0 }
+    while (true) {
+        val firstEmpty = result.firstOrNull { it.id == -1 } ?: return result
+        val firstIndex = result.indexOf(firstEmpty)
+        val last = result.last()
+        result = if (last.n < firstEmpty.n) {
+            result.subList(0, firstIndex) +
+                    Data(firstEmpty.start, last.id, last.n) +
+                    Data(firstEmpty.start + last.n, -1, firstEmpty.n - last.n) +
+                    result.subList(firstIndex + 1, result.size - 1)
+        } else if (last.n == firstEmpty.n) {
+            result.subList(0, firstIndex) +
+                    Data(firstEmpty.start, last.id, last.n) +
+                    result.subList(firstIndex + 1, result.size - 1)
+        } else {
+            // split last block
+            result.subList(0, firstIndex) +
+                    Data(firstEmpty.start, last.id, firstEmpty.n) +
+                    result.subList(firstIndex + 1, result.size - 1) +
+                    Data(last.start, last.id, last.n - firstEmpty.n)
+        }
+        result = result.dropLastWhile { it.id == -1 }
+    }
+}
 
 // part 2
 
-fun List<String>.part2() = 0
+fun List<String>.part2(): Long {
+    return first().parse().compactify2().sumOf { it.checksum }
+}
+
+fun List<Data>.compactify2(): List<Data> {
+    var id = last().id
+    var result = this.filter { it.n > 0 }
+    while (id >= 0) {
+        val lastIndex = result.indexOfLast { it.id == id }
+        val last = result[lastIndex]
+        val firstEmptySpace = result.indexOfFirst { it.id == -1 && it.n >= last.n }
+        if (firstEmptySpace != -1 && firstEmptySpace < lastIndex) {
+            // move last block entirely into the empty space
+            // keep empty space no matter how small
+            val empty = result[firstEmptySpace]
+            result = result.subList(0, firstEmptySpace) +
+                    Data(empty.start, id, last.n) +
+                    Data(empty.start + last.n, -1, empty.n - last.n) +
+                    result.subList(firstEmptySpace + 1, lastIndex) +
+                    result.subList(lastIndex + 1, result.size)
+        }
+        id--
+    }
+    return result
+}
 
 // calculate answers
 
