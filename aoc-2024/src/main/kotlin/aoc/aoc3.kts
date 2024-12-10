1be6853328ff4873b464b55a5d7be05887b612ea
Elisha Peterson <elisha.peterson@jhuapl.edu>
2024-12-10 10:36:32 -0500

    merge

--- aoc-2024/src/main/kotlin/aoc/aoc3.kts
+++ aoc-2024/src/main/kotlin/aoc/aoc3.kts
@@ -3,16 +3,30 @@ import aoc.*
 import aoc.getDayInput
 
 val testInput = """
-
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 """.parselines
 
 // part 1
 
-fun List<String>.part1(): Int = 0
+val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+
+fun List<String>.part1(): Int {
+    var sum = 0
+    forEach { line ->
+        regex.findAll(line).forEach { match ->
+            val (a, b) = match.destructured
+            sum += a.toInt() * b.toInt()
+        }
+    }
+    return sum
+}
 
 // part 2
 
-fun List<String>.part2() = 0
+fun List<String>.part2(): Int {
+    val doStatements = joinToString("").splitOn("do()") { it.substringBefore("don't()") }
+    return doStatements.part1()
+}
 
 // calculate answers
 
