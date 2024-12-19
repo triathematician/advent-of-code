e99846d8fce4a7f993929665b893afc8cb3ae5d5
Elisha Peterson <triathematician@gmail.com>
2024-12-19 07:27:01 -0500

    aoc-2024-day-19

--- aoc-2024/src/main/kotlin/aoc/aoc19.kts
+++ aoc-2024/src/main/kotlin/aoc/aoc19.kts
@@ -4,16 +4,61 @@ import aoc.getDayInput
 import aoc.util.*
 
 val testInput = """
+r, wr, b, g, bwu, rb, gb, br
 
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
 """.parselines
 
 // part 1
 
-fun List<String>.part1(): Int = 0
+class Challenge(input: List<String>) {
+    val patterns = input[0].split(", ")
+    val designs = input.drop(2)
+
+    val cache = mutableMapOf<String, Boolean>()
+    val cacheWays = mutableMapOf<String, Long>()
+
+    fun feasibleDesign(d: String): Boolean {
+        if (d in cache) return cache[d]!!
+        val result = patterns.any {
+            d == it || (d.startsWith(it) && feasibleDesign(d.drop(it.length)))
+        }
+        cache[d] = result
+        return result
+    }
+
+    fun feasibleDesignWays(d: String): Long {
+        if (d in cacheWays) return cacheWays[d]!!
+        val result = patterns.sumOf {
+            when {
+                d == it -> 1L
+                d.startsWith(it) -> feasibleDesignWays(d.drop(it.length))
+                else -> 0L
+            }
+        }
+        cacheWays[d] = result
+        return result
+    }
+}
+
+fun List<String>.part1() =
+    Challenge(this).let { ch ->
+        ch.designs.count { ch.feasibleDesign(it) }
+    }
 
 // part 2
 
-fun List<String>.part2() = 0
+fun List<String>.part2() =
+    Challenge(this).let { ch ->
+        ch.designs.sumOf { ch.feasibleDesignWays(it) }
+    }
 
 // calculate answers
 
