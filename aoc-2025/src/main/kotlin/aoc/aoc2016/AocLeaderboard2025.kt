208f125918d21dab1573188d57d8cf4a4256e3a0
Elisha Peterson <triathematician@gmail.com>
2025-12-01 11:19:35 -0500

    aoc2025-day0

---
+++ aoc-2025/src/main/kotlin/aoc/aoc2016/AocLeaderboard2025.kt
@@ -0,0 +1,16 @@
+package aoc.aoc2016
+
+import aoc.report.AocStats.printStats
+import aoc.report.AocSite.resourceFile
+import aoc.report.printLeaderboard
+
+object AocLeaderboard2025 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        printLeaderboard(2025)
+        // read personal times from text
+        val personalTimesText = resourceFile(2025, "site/stats/personaltimes.txt").readText()
+            .substringAfter("Part 2").substringAfter("\n").trim()
+        printStats(personalTimesText)
+    }
+}
---
+++ aoc-2025/src/main/kotlin/aoc/aoc2025/AocDay1.kt
@@ -0,0 +1,14 @@
+package aoc.aoc2025
+
+import aoc.AocDay
+
+class AocDay1: AocDay(0, 2025) {
+    companion object { @JvmStatic fun main(args: Array<String>) { AocDay1().run() } }
+
+    override val testinput = """
+        
+    """.trimIndent().lines()
+
+    override fun calc1(input: List<String>) = null
+    override fun calc2(input: List<String>) = null
+}
